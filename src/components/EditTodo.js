be2dafc8e19d735929ba5640a9108dd39924aafc
rilangkishabong <rilangkishabong@gmail.com>
2021-03-22 00:33:43 +0530

    created the frontend part of perntodo.

---
+++ src/components/EditTodo.js
@@ -0,0 +1,87 @@
+import React, { Fragment, useState } from "react";
+
+export const EditTodo = ({ todo }) => {
+  console.log(todo);
+  const [description, setDescription] = useState(todo.description);
+
+  const updateTodo = async (e) => {
+    e.preventDefault();
+    try {
+      const body = { description };
+      const response = await fetch(
+        `http://localhost:5000/todos/${todo.todo_id}`,
+        {
+          method: "PUT",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(body),
+        }
+      );
+      window.location = "/";
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
+  return (
+    <Fragment>
+      <button
+        type="button"
+        class="btn btn-warning"
+        data-toggle="modal"
+        data-target={`#${todo.todo_id}`}
+      >
+        Edit
+      </button>
+
+      <div
+        class="modal"
+        id={`${todo.todo_id}`}
+        onClick={() => setDescription(todo.description)}
+      >
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h4 class="modal-title">Edit Todo</h4>
+              <button
+                type="button"
+                class="close"
+                data-dismiss="modal"
+                onClick={() => setDescription(todo.description)}
+              >
+                &times;
+              </button>
+            </div>
+
+            <div class="modal-body">
+              <input
+                type="text"
+                onChange={(e) => setDescription(e.target.value)}
+                className="form-control"
+                value={description}
+              />
+            </div>
+
+            <div class="modal-footer">
+              <button
+                type="button"
+                class="btn btn-warning"
+                data-dismiss="modal"
+                onClick={updateTodo}
+              >
+                Edit
+              </button>
+              <button
+                type="button"
+                class="btn btn-danger"
+                data-dismiss="modal"
+                onClick={() => setDescription(todo.description)}
+              >
+                Close
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Fragment>
+  );
+};
---
+++ src/components/ListTodo.js
@@ -0,0 +1,70 @@
+import React, { Fragment, useState, useEffect } from "react";
+import { EditTodo } from "./EditTodo";
+
+export const ListTodo = () => {
+  const [todos, setTodos] = useState([]);
+
+  const getTodos = async () => {
+    try {
+      const response = await fetch("http://localhost:5000/todos");
+      const jsonData = await response.json();
+      console.log(jsonData);
+      setTodos(jsonData);
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
+  useEffect(() => {
+    getTodos();
+  }, []);
+
+  const deleteTodo = async (id) => {
+    try {
+      const deleteTodo = await fetch(`http://localhost:5000/todos/${id}`, {
+        method: "DELETE",
+      });
+      console.log(deleteTodo);
+      setTodos(todos.filter((todo) => todo.todo_id !== id));
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
+  return (
+    <Fragment>
+      <table class="table mt-5 text-center">
+        <thead>
+          <tr>
+            <th>Description</th>
+            <th>Edit</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          {todos.map((todo) => {
+            return (
+              <tr key={todo.todo_id}>
+                <td>{todo.description}</td>
+                <td>
+                  <EditTodo todo={todo} />
+                </td>
+                <td>
+                  <button
+                    className="btn btn-danger"
+                    onClick={() => {
+                      deleteTodo(todo.todo_id);
+                    }}
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            );
+          })}
+          <tr></tr>
+        </tbody>
+      </table>
+    </Fragment>
+  );
+};
